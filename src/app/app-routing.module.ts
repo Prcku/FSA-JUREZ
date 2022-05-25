7ae6e438bd269480db931744e91d6050c16ff98d
branislav <branislavsocha159@gmail.com>
2022-05-25 15:29:25 +0200

    funkcna aplikacia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CalcComponent} from "./calc/calc.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {TodoComponent} from "./todo/todo.component";
+import {CoursesComponent} from "./courses/courses.component";
+import {NewCourseComponent} from "./new-course/new-course.component";
+import {TeachersComponent} from "./teachers/teachers.component";
+import {CourseDetailComponent} from "./course-detail/course-detail.component";
+import {EditCourseComponent} from "./edit-course/edit-course.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'calc', component: CalcComponent},
+  {path:'todo', component: TodoComponent},
+  {path:'courses', component: CoursesComponent},
+  {path:'courses/new', component: NewCourseComponent},
+  {path:'courses/:id', component: CourseDetailComponent},
+  {path:'courses/:id/edit', component: EditCourseComponent},
+  {path:'teachers', component: TeachersComponent},
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { CalcComponent } from './calc/calc.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TodoComponent } from './todo/todo.component';
+import {LocalStorageModule} from "angular-2-local-storage";
+import {registerLocaleData} from "@angular/common";
+import localeSk from '@angular/common/locales/sk';
+import { TodoItemPipe } from './todo-item.pipe';
+import { CoursesComponent } from './courses/courses.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { NewCourseComponent } from './new-course/new-course.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { EditCourseComponent } from './edit-course/edit-course.component'
+import {HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
+
+registerLocaleData(localeSk)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
+    HomeComponent,
+    NotFoundComponent,
+    TodoComponent,
+    TodoItemPipe,
+    CoursesComponent,
+    TeachersComponent,
+    NewCourseComponent,
+    CourseDetailComponent,
+    EditCourseComponent,
+    LoginComponent
+
+
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    LocalStorageModule.forRoot({
+      prefix: 'my-first-app',
+      storageType: 'localStorage'
+    })
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'sk'}
+  ],
+  // HttpClientModule,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
